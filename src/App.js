3ec649c1f46b32f523c504c96d1ff8eeb33800a6
IgnaDeveloper <ignadeveloper@gmail.com>
2023-01-25 19:12:51 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logoMaestroRoshi from "./images/logo.png";
+import Boton from "./Componentes/Boton.js";
+import Contador from "./Componentes/Contador";
+import { useState } from "react";
 
 function App() {
+  const [numClics, setNumClics] = useState(0);
+
+  const manejarClic = () => {
+    setNumClics(numClics + 1);
+  };
+  const reiniciarContador = () => {
+    setNumClics(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="logo-container">
+        <div className="principal-container">
+          <img
+            className="logo"
+            src={logoMaestroRoshi}
+            alt="logo maestro roshi"
+          />
+          <Contador numClics={numClics} />
+          <Boton texto="Clic" esBotonDeClic={true} manejarClic={manejarClic} />
+          <Boton
+            texto="Reiniciar"
+            esBotonDeClic={false}
+            manejarClic={reiniciarContador}
+          />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Componentes/Contador.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Contador({ numClics }) {
+  return <div className="contador">{numClics}</div>;
+}
